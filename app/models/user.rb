dcaaa7c1850a8b68b6e581376b30f19c3008bdd8
yungcheng <yungcheng111023@gmail.com>
2018-01-14 21:16:13 +0800

    setup relation among user,restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
 
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   
   def admin?
     self.role == "admin"
