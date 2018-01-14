a3fc3fc835d21778ce5b69e00207b14aadbbb0d1
yungcheng <yungcheng111023@gmail.com>
2018-01-14 21:48:06 +0800

    add favorite/unfavorite button on index page using shared partial and set_restaurant to controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :dashboard, :update, :favorite, :unfavorite]
 
   def index
     @restaurants = Restaurant.page(params[:page]).per(9)
@@ -6,7 +7,7 @@ class RestaurantsController < ApplicationController
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])  
+     
     @comment = Comment.new
   end
 
@@ -16,21 +17,24 @@ class RestaurantsController < ApplicationController
   end
 
   def dashboard
-    @restaurant = Restaurant.find(params[:id])
   end
 
   # POST /restaurants/:id/favorite
   def favorite
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.favorites.create!(user: current_user)
     redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
   def unfavorite
-    @restaurant = Restaurant.find(params[:id])
     favorites = Favorite.where(restaurant: @restaurant, user: current_user)
     favorites.destroy_all
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
