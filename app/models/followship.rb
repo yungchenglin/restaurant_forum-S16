5cd6d0e923d6fee319955dc2b89a2113f0df398d
yungcheng <yungcheng111023@gmail.com>
2018-01-20 09:49:35 +0800

    add following? method to user model for checking follow/unfollow button

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -3,7 +3,7 @@ class Followship < ApplicationRecord
    validates :following_id, uniqueness: { scope: :user_id }
 
    belongs_to :user
-   belongs_to :follwing, class_name: "User"
+   belongs_to :following, class_name: "User"
 
 
 end
