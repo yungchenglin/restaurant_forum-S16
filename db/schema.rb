b4282e23a55c4984928a100fe03c0021cddba11d
yungcheng <yungcheng111023@gmail.com>
2018-01-21 09:47:39 +0800

    create friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180114151037) do
+ActiveRecord::Schema.define(version: 20180121014425) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20180114151037) do
   create_table "followships", force: :cascade do |t|
     t.integer "user_id"
     t.integer "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
