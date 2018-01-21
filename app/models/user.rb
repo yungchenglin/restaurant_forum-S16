d5d2da96fad52025e9a71db8083b151bacc07ecb
yungcheng <yungcheng111023@gmail.com>
2018-01-21 09:54:25 +0800

    setup self-referential relationships within users table through friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,9 @@ class User < ApplicationRecord
 
   has_many :inverse_followships, class_name: "Followship", foreign_key: "following_id"
   has_many :followers, through: :inverse_followships, source: :user
+
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
   
   def admin?
     self.role == "admin"
